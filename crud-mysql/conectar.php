daf17727da68942a1164a8128e4b33218b787665
Edmarcos de araujo <edmarcos1993@hotmail.com>
2023-04-09 20:03:23 -0300

    formulario crud-mysql

---
+++ crud-mysql/conectar.php
@@ -0,0 +1,62 @@
+<?php
+
+function conectar(){
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$db = "mydb";
+$con = new mysqli($servidor, $usuario, $senha,"mydb");
+
+if($con->connect_error){
+die("Erro :".$con->connect_error);
+}
+//echo "ok ao conectar";
+return $con;
+}
+
+function incluir($nome, $email, $cpf){
+     $con = conectar();
+    $sql = "insert into pessoa (nome, email, cpf) values ('$nome','$email','$cpf');";
+if($con->query($sql) === True){
+    return"ok ao gravar";
+}else{
+  return "Erro: $sql".$con->error; 
+}
+}
+function listar(){
+    $con = conectar();
+    $sql = "select id, nome, email, cpf from pessoa";
+    $resultado = $con->query($sql);
+    return $resultado;
+}
+
+function buscar($id){
+    $con = conectar();
+    $sql = "select id, nome, email, cpf from pessoa where id = $id";
+    $resultado = $con->query($sql);
+    $resultado = $resultado->fetch_assoc();
+    return $resultado;
+}
+
+function alterar($id, $nome, $email, $cpf){
+    $con = conectar();
+    $sql = "update pessoa set nome = '$nome', email = '$email', cpf = '$cpf' where id = $id";
+    if($con->query($sql) === true){
+        return "Ok ao Atualizar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
+function apagar($id){
+    $con = conectar();
+    $sql = "delete from pessoa where id = $id";
+    if($con->query($sql) === true){
+        return "Ok ao Apagar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
+?>
+
