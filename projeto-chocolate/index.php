2e1e5f70395399abd1e7818e6d713862f5299843
Edmarcos de araujo <edmarcos1993@hotmail.com>
2023-04-27 21:03:33 -0300

    chocolate

---
+++ projeto-chocolate/index.php
@@ -0,0 +1,51 @@
+<html lang= "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equive="X-UA-compatible" content="IE-edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Lojas de doces</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"rel="stylesheet">
+</head>
+<body>
+    <style>
+        a{
+            float: right;
+        }
+        .card{
+            float: left;
+            margin: 10px;
+        }
+        h2{
+            text-align: center;
+        }
+        </style>
+     <div class="container">
+        <h2>Doces de chocolates</h2>
+        <div class="card" style="width: 300px">
+        <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/nutela.webp" alt="" class="card-img-top">
+        <div class="card-body">
+            <h4 class="card-title">Ovo de chocolate com nutela</h4>
+<p class="card-text">R$ 50,00<a href="#" class="btn btn-primary">🛒</a></p>
+</div>
+</div>
+<div class="card" style="width: 300px">
+        <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-maracuja.webp" alt="" class="card-img-top">
+        <div class="card-body">
+            <h4 class="card-title">Ovo de brigadeiro de maracuja</h4>
+<p class="card-text">R$ 50,00<a href="#" class="btn btn-primary">🛒</a></p>
+</div>
+</div>
+<div class="card" style="width: 300px">
+        <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-limao.webp" alt="" class="card-img-top">
+        <div class="card-body">
+            <h4 class="card-title">Ovo de brigadeiro com limao</h4>
+<p class="card-text">R$ 50,00<a href="#" class="btn btn-primary">🛒</a></p>
+</div>
+</div>
+<div class="card" style="width: 300px">
+        <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/chocolate-oreo.webp" alt="" class="card-img-top">
+        <div class="card-body">
+            <h4 class="card-title">Ovo de chocolate com oreo</h4>
+<p class="card-text">R$ 50,00<a href="#" class="btn btn-primary">🛒</a></p>
+</div>
+</div>
